e3f40837aec14e46f52f771aef808bb5e52699fb
orca911 <onigiri.yar@gmail.com>
2022-06-01 15:22:33 +0900

    あ

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ function scroll_effect() {
 
 
 var swiper = new Swiper(".mySwiper", {
-  effect: "flip",
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
