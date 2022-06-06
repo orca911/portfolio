a28cb6de9690a5134e146ea6659137682d8f1b7f
orca911 <onigiri.yar@gmail.com>
2022-06-06 14:22:25 +0900

    画像削除　フォルダ整理　works実装

--- script.js
+++ script.js
@@ -8,16 +8,16 @@
 var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
 		if (windowwidth > 768){
 			var responsiveImage = [//PC用の画像
-				{ src: 'images/main1.jpg'},
-				{ src: 'images/neko.jpg'},
-				{ src: 'images/twilight.jpg'},
-				{ src: 'images/matu.jpg'}
+				{ src: 'images/pc-vegas/main1.jpg'},
+				{ src: 'images/pc-vegas/neko.jpg'},
+				{ src: 'images/pc-vegas/twilight.jpg'},
+				{ src: 'images/pc-vegas/matu.jpg'}
 			];
 		} else {
-			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
-				{ src: 'images/SPmain1.jpg'},
-				{ src: 'images/SPmain2.jpg'},
-				{ src: 'images/SPmain3.jpg'}
+			var responsiveImage = [//600px以下用の画像
+				{ src: 'images/sp-vegas/SPmain1.jpg'},
+				{ src: 'images/sp-vegas/SPmain2.jpg'},
+				{ src: 'images/sp-vegas/SPmain3.jpg'}
 			];
 		}
 
@@ -25,7 +25,7 @@ $(function () {
 	$('#vegas').vegas({
 		slides: responsiveImage,//画像設定を読む
 		animation: 'kenburns',
-		transition: 'blur', //切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+		transition: 'blur', //切り替わりのアニメーション。
 		transitionDuration: 2000, //切り替わりのアニメーション時間をミリ秒単位で設定
 		delay: 5000, //スライド間の遅延をミリ秒単位で。
 		animationDuration: 20000, //スライドアニメーション時間をミリ秒単位で設定
